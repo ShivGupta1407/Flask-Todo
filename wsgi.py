08816269673aa58341201bc430f01fe2558e6fa1
ShivGupta1407 <shivgupta.cse14@gmail.com>
2024-04-06 00:38:23 +0530

    update

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,9 @@ from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
+else:
+    # This block is executed when running under a WSGI server like Gunicorn
+    # Use Gunicorn to serve the Flask application
+    from gunicorn.app.wsgiapp import WSGIApplication
+    application = WSGIApplication()
+    application.run()
